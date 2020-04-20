39f70a84261a8375c66274e761fa6a9ea5317478
lexto92@gmail.com <lexto92@gmail.com>
2020-04-20 19:07:15 +0300

    done 5 lecture

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import {render, RenderPosition} from './components/utils.js';
 import BoardController from './controllers/board.js';
 
 const siteMainElement = document.querySelector(`.main`);
+
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, new SiteMenu(), RenderPosition.BEFOREEND);
@@ -13,8 +14,11 @@ render(siteHeaderElement, new SiteMenu(), RenderPosition.BEFOREEND);
 render(siteMainElement, new Filter(filters), RenderPosition.BEFOREEND);
 
 const boardComponent = new Board();
+
 const boardConroller = new BoardController(boardComponent);
+
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
+
 boardConroller.render(dataCards);
 
 
